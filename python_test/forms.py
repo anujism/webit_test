498e8775b9889654b5a8a1ea53a17e5320ccab14
Anuj Agarwal <anuj.ism@gmail.com>
2018-03-21 14:18:09 +0530

    Add * on form fields

--- python_test/forms.py
+++ python_test/forms.py
@@ -1,14 +1,37 @@
 from django import forms
 
 from .models import Client
+from django.utils.safestring import mark_safe
+
+import string
+
+
+def pretty_name(name):
+    if not name:
+        return ''
+    return string.capwords(name.replace('_', ' '))
 
 
 class BootstrapForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(BootstrapForm, self).__init__(*args, **kwargs)
+        self.stylize_fields()
 
-        for key in self.fields:
-            self.fields[key].widget.attrs['class'] = 'form-control'
+    def stylize_fields(self):
+        # Add classes for the HTML template in use
+        for name, field in self.fields.items():
+
+            if getattr(field, '_b3_stylized', False):
+                continue
+
+            field.widget.attrs['class'] = 'form-control'
+            field.label = pretty_name(field.label or name)
+
+            if field.required:
+                field.widget.attrs['required'] = 'required'
+                field.label = mark_safe('<span class="required">*</span> {}'.format(field.label))
+
+            field._b3_stylized = True
 
 
 class ClientSearchForm(BootstrapForm):
@@ -17,10 +40,10 @@ class ClientSearchForm(BootstrapForm):
         fields = ['name', 'email', 'phone', 'suburb']
 
     def __init__(self, *args, **kwargs):
-        super(ClientSearchForm, self).__init__(*args, **kwargs)
+        for key in self.base_fields:
+            self.base_fields[key].required = False
 
-        for key in self.fields:
-            self.fields[key].required = False
+        super(ClientSearchForm, self).__init__(*args, **kwargs)
 
 
 class ClientForm(BootstrapForm):
